777334026bec0556ab381a9954ac547063b837b0
soave424 <soave424@gmail.com>
2024-08-25 09:03:36 +0900

    error

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 import streamlit as st
-from google_play_scraper import app
+from google_play_scraper import app, NotFoundError
 
-# 스트림릿 앱의 타이틀 설정
 st.title('Google Play Store App Searcher')
 
-# 사용자 입력 받기
 app_name = st.text_input('Enter the name of the app to search for:')
 
-# 검색 버튼
 if st.button('Search'):
-    # 구글 플레이 스토어에서 앱 검색
-    result = app(app_name, lang='ko', country='kr')  # 앱 이름, 언어 및 국가 설정
-    if result:
+    try:
+        # 구글 플레이 스토어에서 앱 검색
+        result = app(app_name, lang='ko', country='kr')
         # 검색 결과 표시
         st.write(f"**App Name:** {result['title']}")
         st.write(f"**Developer:** {result['developer']}")
         st.write(f"**Rating:** {result['score']}")
         st.write(f"**Download Link:** [Link](https://play.google.com/store/apps/details?id={result['appId']})")
-    else:
-        st.write('No results found.')
+    except NotFoundError:
+        st.error('App not found. Please check the name and try again.')

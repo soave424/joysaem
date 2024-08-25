fcfa8fc9dcb93851639e6fe30d4c4601045846d9
soave424 <soave424@gmail.com>
2024-08-25 17:41:58 +0900

    edit

--- pages/03_fineconbooks.py
+++ pages/03_fineconbooks.py
@@ -22,13 +22,21 @@ def fetch_books_from_url(base_url, start_page, end_page):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         for item in soup.find_all('div', class_='goods_info'):
-            title = item.find('a', class_='gd_name').text.strip()
-            author = item.find('span', class_='authPub info_auth').text.strip()
-            publisher = item.find('span', class_='authPub info_pub').text.strip()
-            pub_date = item.find('span', class_='authPub info_date').text.strip()
-            price = item.find('span', class_='priceB').text.strip()
-            link = item.find('a', class_='gd_name')['href']
-            image_url = item.find('img', class_='goods_img')['src']
+            title_tag = item.find('a', class_='gd_name')
+            if not title_tag:
+                continue  # 제목 태그가 없으면 다음 아이템으로 넘어갑니다.
+            title = title_tag.text.strip()
+            author_tag = item.find('span', class_='authPub info_auth')
+            author = author_tag.text.strip() if author_tag else "Author not available"
+            publisher_tag = item.find('span', class_='authPub info_pub')
+            publisher = publisher_tag.text.strip() if publisher_tag else "Publisher not available"
+            pub_date_tag = item.find('span', class_='authPub info_date')
+            pub_date = pub_date_tag.text.strip() if pub_date_tag else "Date not available"
+            price_tag = item.find('span', class_='priceB')
+            price = price_tag.text.strip() if price_tag else "Price not available"
+            link = title_tag['href'] if title_tag else "#"
+            image_tag = item.find('img', class_='goods_img')
+            image_url = image_tag['src'] if image_tag else "No image available"
 
             books.append({
                 'Title': title,
@@ -41,6 +49,7 @@ def fetch_books_from_url(base_url, start_page, end_page):
             })
 
     return books
+
 
 st.title('Yes24 Book Scraper')
 

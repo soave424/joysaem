e737e82cf5fb63c61e852b932bfff034eaeb9694
soave424 <soave424@gmail.com>
2024-08-25 09:25:30 +0900

    except delete

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from google_play_scraper import app, search
 from google_play_scraper.exceptions import NotFoundError
 from urllib.parse import quote_plus
 
-
 st.title('Google Play Store App Searcher')
 
 app_name_query = st.text_input('Enter the name of the app to search for:')
@@ -40,5 +39,5 @@ if st.button('Search'):
         st.write(f"**Download Link:** [Link](https://play.google.com/store/apps/details?id={app_id})")
     except NotFoundError:
         st.error('App not found. Please check the name and try again.')
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+    # except Exception as e:
+    #     st.error(f"An error occurred: {str(e)}")

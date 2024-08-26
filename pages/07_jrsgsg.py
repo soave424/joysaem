1281acdccbec99b542ccbec19e395e7f3c4d1e36
soave424 <soave424@gmail.com>
2024-08-26 20:26:20 +0900

    s

--- pages/07_jrsgsg.py
+++ pages/07_jrsgsg.py
@@ -14,16 +14,14 @@ if 'show_pdf' not in st.session_state:
 if base_url:
     # Create buttons for each PDF number from 01 to 16
     for i in range(1, 17):
-        button_label = f"View PDF {i:02}"
+        button_label = f"{i:02}"
         if st.button(button_label):
             st.session_state['show_pdf'][i] = not st.session_state['show_pdf'][i]
         
         # If the corresponding button was pressed, show or hide the PDF
         if st.session_state['show_pdf'][i]:
             pdf_url = f"{base_url}{i:02}.pdf"
-            st.write(f"Displaying PDF {i:02}")
             st.markdown(f'<iframe src="{pdf_url}" width="700" height="1000" type="application/pdf"></iframe>', unsafe_allow_html=True)
-        else:
-            st.write(f"PDF {i:02} is hidden")
+
 else:
     st.write("Please enter a base URL to display the PDFs.")

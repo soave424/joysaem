3fda4bad789af6f43ab36d52015e00f0393c0b4a
soave424 <soave424@gmail.com>
2024-08-31 22:28:35 +0900

    a

--- pages/12_urljson.py
+++ pages/12_urljson.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import json
 
 # URL 입력 받기
-st.title("UL-LI 요소 추출기")
+st.title("LI 요소 추출기")
 url = st.text_input("URL을 입력하세요:")
 
 # 사용자가 URL을 입력하고 버튼을 누르면 실행
@@ -18,18 +18,30 @@ if st.button("추출"):
             # BeautifulSoup을 사용하여 HTML 파싱
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            # ul 요소를 찾고 그 안의 li 요소들을 찾음
-            ul_elements = soup.find_all('ul')
-            all_li_elements = []
+            # 모든 ul 또는 ol 요소를 찾음
+            lists = soup.find_all(['ul', 'ol'])
 
-            for ul in ul_elements:
-                li_elements = ul.find_all('li')
-                for li in li_elements:
-                    li_text = li.get_text(strip=True)
-                    all_li_elements.append(li_text)
+            # 결과를 저장할 딕셔너리
+            result = {}
+
+            for list_tag in lists:
+                # 클래스명 또는 id를 가져옴
+                title = list_tag.get('class', list_tag.get('id', 'unnamed_list'))
+
+                if isinstance(title, list):
+                    title = ' '.join(title)
+                elif not title:
+                    title = 'unnamed_list'
+
+                # li 요소의 텍스트를 리스트로 저장
+                li_elements = list_tag.find_all('li')
+                li_texts = [li.get_text(strip=True) for li in li_elements]
+
+                # 결과 딕셔너리에 저장
+                result[title] = li_texts
 
             # JSON 형식으로 변환
-            json_output = json.dumps(all_li_elements, ensure_ascii=False, indent=4)
+            json_output = json.dumps(result, ensure_ascii=False, indent=4)
 
             # 결과 출력
             st.json(json_output)

95af95a9005558f3c2a429f9abf18b4535ee0790
soave424 <soave424@gmail.com>
2025-05-03 11:30:39 +0900

    a'

--- pages/22_wordspeaking.py
+++ pages/22_wordspeaking.py
@@ -179,15 +179,12 @@ html_code = """
 
 html(html_code, height=750)
 
-# 단어 입력 학습 UI
-
-
 # 단어 학습 결과 출력
 st.markdown("### 📚 단어 학습 결과")
-col1, col2 = st.columns([1, 2])
+col1, col2 = st.columns(2, gap="large")
 with col1:
-    st.markdown("** 학습할 단어 입력**")
-    input_word = st.text_input("",key="input_word_field")
+    st.markdown("#### 📝 단어 직접 입력 학습")
+    input_word = st.text_input("학습할 단어 입력", key="input_word_field")
 
     if input_word and input_word != st.session_state.input_word:
         st.session_state.input_word = input_word
@@ -195,9 +192,8 @@ with col1:
         if input_word not in st.session_state.word_history:
             st.session_state.word_history.append(input_word)
 
-
 with col2:
-    st.markdown("**번역 결과**")
+    st.markdown("#### 📖 번역 결과")
     st.code(st.session_state.translated or "(단어를 입력하면 번역이 표시됩니다)", language="text")
 
 # 학습한 단어 목록

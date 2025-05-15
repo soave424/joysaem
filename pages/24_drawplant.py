1de42de50364c334cff8fec3c7393a68c2f904b8
soave424 <soave424@gmail.com>
2025-05-15 22:40:15 +0900

    a

--- pages/24_drawplant.py
+++ pages/24_drawplant.py
@@ -1,22 +1,6 @@
 import streamlit as st
-import requests
 
 # Streamlit secrets에서 API 키 불러오기
-API_KEY = st.secrets["PlankDraw_API_Key"]
+API_KEY = st.secrets["PlankDraw_API_Key"]["key"]
 
-# API 호출 함수 예시
-def search_plants(keyword, page=1):
-    API_URL = "http://apis.data.go.kr/1400119/PlantMiniService/miniatureSearch"
-    params = {
-        'serviceKey': API_KEY,  # secrets에서 불러온 API 키 사용
-        'st': 1,  # 1: 국명으로 검색, 2: 학명으로 검색
-        'sw': keyword,  # 검색어
-        'numOfRows': 10,  # 페이지당 결과 수
-        'pageNo': page,  # 페이지 번호
-    }
-    response = requests.get(API_URL, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error(f"HTTP 오류: {response.status_code}")
-        return None
+st.write(f"API Key: {API_KEY}")  # 정상적으로 API Key가 불러와지는지 확인

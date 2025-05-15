1332bd60e570ddbb8ae2857faa8644e066872fd7
soave424 <soave424@gmail.com>
2025-05-15 22:40:48 +0900

    a

--- pages/24_drawplant.py
+++ pages/24_drawplant.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 # Streamlit secrets에서 API 키 불러오기
-API_KEY = st.secrets["PlankDraw_API_Key"]["key"]
+API_KEY = st.secrets["PlankDraw_API_Key"]
 
 st.write(f"API Key: {API_KEY}")  # 정상적으로 API Key가 불러와지는지 확인
